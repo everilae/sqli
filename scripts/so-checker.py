25b45a4e58e15dd21c51430f8c4fc09c82419fac
Ilja Everilä <ilja.everila@siili.com>
2017-10-25 10:23:37 +0300

    Parse code blocks separately

--- scripts/so-checker.py
+++ scripts/so-checker.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from textwrap import dedent
 from argparse import ArgumentParser
 from datetime import datetime
+from itertools import chain
 
 _parser = ArgumentParser(description="Find vulnerable Python from SO")
 _parser.add_argument("tags", nargs="*",
@@ -17,12 +18,12 @@ _FILTER = "!5-dm_.B4KdW3(tKMnD-gYaOdS-mkdxhSIbFHRm"
 
 
 def fetch_post_soup(item):
-    body = item["body"]
+    bs = [item["body"]]
 
     if "answers" in item:
-        body += "\n".join(a["body"] for a in item["answers"])
+        bs += [a["body"] for a in item["answers"]]
 
-    return BeautifulSoup(body, "lxml")
+    return (BeautifulSoup(body, "lxml") for body in bs)
 
 
 def fetch(tags):
@@ -41,20 +42,22 @@ def fetch(tags):
     for it in items:
         link = it["link"]
         created = datetime.utcfromtimestamp(it["creation_date"])
-        soup = fetch_post_soup(it)
-        for code in soup.select("code"):
+        soups = fetch_post_soup(it)
+        codes = chain.from_iterable(soup.select("code") for soup in soups)
+        poisoned = []
+        for code in codes:
             source = dedent(code.text)
             try:
-                poisoned = check(source)
-                if poisoned:
-                    print("{:%Y-%m-%d} {}:".format(created, link))
-                    for p in poisoned:
-                        print("#{}: {}".format(p.get_lineno(), p.get_source()))
-                    break
+                poisoned.extend(check(source))
 
             except Exception as e:
                 # Just ignore
                 pass
+
+        if poisoned:
+            print("{:%Y-%m-%d} {}:".format(created, link))
+            for p in poisoned:
+                print("#{}: {}".format(p.get_lineno(), p.get_source()))
 
 if __name__ == "__main__":
     args = _parser.parse_args()
