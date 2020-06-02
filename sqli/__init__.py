47443c1c6dceeb04ad630b481688c83a0014ff3b
Ilja Everilä <ilja.everila@siili.com>
2020-06-02 09:50:57 +0300

    Use NodeTransformer for cure

--- sqli/__init__.py
+++ sqli/__init__.py
@@ -11,18 +11,13 @@ _FORMAT = {"format", "format_map"}
 _log = logging.getLogger(__name__)
 
 
-def _cure(node):
-    if isinstance(node, Poison):
-        node = node.value
+class _Cure(gast.NodeTransformer):
 
-    for name, value in gast.iter_fields(node):
-        if isinstance(value, gast.AST):
-            setattr(node, name, _cure(value))
+    def visit_Poison(self, node):
+        node = self.generic_visit(node)
+        return node.value
 
-        elif isinstance(value, list):
-            setattr(node, name, [_cure(x) for x in value])
-
-    return node
+_cure = _Cure()
 
 
 def _is_str_const(node):
@@ -50,8 +45,7 @@ class Poison(gast.AST):
     _fields = ["value"]
 
     def __init__(self, node):
-        # Unparser is unhappy about custom "AST nodes"
-        self.value = _cure(node)
+        self.value = node
 
     def __repr__(self):
         return "<Poison value={!r}>".format(self.get_source())
@@ -60,8 +54,9 @@ class Poison(gast.AST):
         return self.value.lineno
 
     def get_source(self):
+        node = _cure.visit(self.value)
         try:
-            return astunparse.unparse(gast.gast_to_ast(self.value))
+            return astunparse.unparse(gast.gast_to_ast(node))
 
         except AttributeError:
             _log.debug(gast.dump(self.value))
@@ -82,18 +77,27 @@ class SQLChecker(gast.NodeTransformer):
 
         return value
 
+    def _handle_add(self, node):
+        if not _is_str_const(node.left) or not _is_str_const(node.right):
+            if isinstance(node.left, Poison):
+                node.left = node.left.value
+
+            if isinstance(node.right, Poison):
+                node.right = node.right.value
+
+            node = Poison(node)
+
+        else:
+            node = gast.copy_location(
+                gast.Constant(node.left.value + node.right.value, None),
+                node)
+
+        return node
+
     def visit_BinOp(self, node):
         node = self.generic_visit(node)
         if isinstance(node.op, gast.Add):
-            if not _is_str_const(node.left) or not _is_str_const(node.right):
-                node = Poison(node)
-
-            else:
-                node = gast.copy_location(
-                    gast.Constant(
-                        node.left.value + node.right.value,
-                        None),
-                    node)
+            node = self._handle_add(node)
 
         elif isinstance(node.op, gast.Mod):
             # Treat all % operations as poisonous
